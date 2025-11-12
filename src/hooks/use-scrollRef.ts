06c6168f1b2fbf38fa783938d5e9273e5242428b
Ali X <al00x@outlook.com>
2025-11-12 13:12:10 +0330

    fix: fixed PlanetDivider logic and made it more smooth and optmized

--- src/hooks/use-scrollRef.ts
+++ src/hooks/use-scrollRef.ts
@@ -1,17 +1,36 @@
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef, useCallback } from 'react'
+
+const throttle = <T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): ((...args: Parameters<T>) => void) => {
+  let inThrottle: boolean
+
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+    const context = this
+    if (!inThrottle) {
+      func.apply(context, args)
+      inThrottle = true
+      setTimeout(() => (inThrottle = false), limit)
+    }
+  }
+}
 
 // This one, is used in order to prevent re-renders during scrolling. (uses UseRef instead of UseState)
-export const useScrollRef = () => {
+export const useScrollRef = ({ throttleDelay = 16 }) => {
   const scrollYRef = useRef(0)
 
-  useEffect(() => {
-    const handleScroll = () => {
+  const handleScroll = useCallback(
+    throttle(() => {
       scrollYRef.current = window.scrollY
-    }
+    }, throttleDelay),
+    []
+  )
 
+  useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true })
     return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+  }, [handleScroll])
 
   return scrollYRef
 }
